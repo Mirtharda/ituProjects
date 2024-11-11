d0b6ab3cb99cd8fb9c308d7b60167ac154984d12
Mirtharda <selinyilmaz1854@gmail.com>
2024-11-11 12:35:05 +0300

    Update blg212

--- fall24/blg212/recits/recit2/Recitation2_Q4.s
+++ fall24/blg212/recits/recit2/Recitation2_Q4.s
@@ -29,23 +29,29 @@ endcopy MOVS 	r0,#0							;i=0 as index value
 		MOVS	r3,#ArraySize					;Load array size
 		SUBS	r3,r3,#4						;size=SIZE-1
 		LDR 	r2,=y_array						;Load start address of the allocated space for y
+		
+		;;outer while loop
 L1		CMP		r0,r3							;Check i<array_size
 		BGE		stop							;if not finish loop
 		MOVS	r1,#0							;j=1 as second index
 		MOVS	r4,r3							;cond=size
 		SUBS	r4,r4,r0						;cond = size-1
+		
+		;;inner while loop
 L2		CMP		r1,r4							;check j< cond
 		BGE		EndL2							;if j >= cond, finish inner loop 
 		LDR		r5,[r2,r1]						;firstval = y[j]
 		ADDS	r1,r1,#4						;j=j+4
-		LDR		r6,[r2,r1]						;secondval = j[i]
+		LDR		r6,[r2,r1]						;secondval = y[j]
+		;;if statement
 		CMP		r5,r6							;check firstval > secondval
-		BLE		L2								;if firstval<=second then jump L1
+		BLE		L2								;if firstval<=second then jump L2
 		STR		r5,[r2,r1]						;y[j]=firstval
 		SUBS	r1,r1,#4						;j=j-4
 		STR		r6,[r2,r1]						;y[j]=secondval
 		ADDS	r1,r1,#4						;j=j+4
 		B		L2								;Go to L2
+		
 EndL2	ADDS	r0,r0,#4						;i=i+4 for word.
 		B		L1								;Go to L1
 		
