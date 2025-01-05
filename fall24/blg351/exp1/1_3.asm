b3bfc71e8131a52bd22eb773c084e7d7749317f9
Selin Yılmaz <selinyilmaz1854@gmail.com>
2025-01-05 15:31:11 +0300

    Update 1_3.asm

--- fall24/blg351/exp1/1_3.asm
+++ fall24/blg351/exp1/1_3.asm
@@ -25,42 +25,36 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 
 SetupP1		mov.b	#00000000b, &P2SEL
-			;mov.b	#00000000b,&P2IN
-			mov.b	#11111111b,&P1DIR
-			mov.b	#11110111b,&P2DIR		;input port
-			mov.b	#00000000b,&P1OUT
-			mov.b	#00000000b,&P2OUT		;set pullup resistor
-			mov.b	#10000000b, R6
+		;mov.b	#00000000b,&P2IN
+		mov.b	#11111111b,&P1DIR
+		mov.b	#11110111b,&P2DIR	; input port
+		mov.b	#00000000b,&P1OUT
+		mov.b	#00000000b,&P2OUT	; set pullup resistor
+		mov.b	#10000000b, R6
+		mov.b	#0d, R8
 
-			mov.b	#0d, R8
-
-			;is.b   #00000001b, &P2REN     ; Enable pull-up/pull-down resistor on P2.0
+		;is.b   #00000001b, &P2REN     ; Enable pull-up/pull-down resistor on P2.0
 
 Mainloop1	bis.b	R6, &P1OUT
+		inc 	R8
+		rra	R6
 
-			inc 	R8
-			rra		R6
+		mov.w	#00500000, R15
+L1		cmp 	#00001000b, &P2IN
+		jeq	stop
+		dec.w 	R15
+		jnz	L1
+		cmp	#8d, R8
+		jeq	SetupP1
+		jmp	Mainloop1
 
-			mov.w	#00500000, R15
-L1			cmp 	#00001000b, &P2IN
-			jeq		stop
-			dec.w 	R15
+stop		;mov.b	#00000000b, &P1OUT	; turn all leds off
+		nop
+		jmp	stop
 
-			jnz		L1
-
-			cmp		#8d, R8
-			jeq		SetupP1
-			jmp		Mainloop1
-
-stop		;mov.b	#00000000b, &P1OUT		;turn all leds off
-			nop
-			jmp		stop
-
-			mov.w #00500000, R15
-l3			dec.w R15
-			jnz l3
-
-
+		mov.w #00500000, R15
+l3		dec.w R15
+		jnz l3
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
